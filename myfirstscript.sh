5f69114cadf597f3ee49d589ff2a318c8ae79644
choncey <nchoncey@gmail.com>
2023-11-27 13:09:38 -0800

    gnupq-apt

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,4 +1,5 @@
 # Keep going bro!
 sudo apt install apache2 -y
 sudo apt install mysql-server-8.0 -y
-sudo apt install postgresql-server-dev-all
+sudo apt install postgresql-server-dev-all -y
+sudo apt instal gnupg -y
